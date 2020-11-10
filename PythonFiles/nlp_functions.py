59b65057ef676041fc39843bf8317c9e9756ad0b
Yousef <namiyousef@hotmail.com>
2020-11-10 17:57:07 +0100

    added stopwords removal, improved whitespace removal

--- PythonFiles/nlp_functions.py
+++ PythonFiles/nlp_functions.py
@@ -5,7 +5,10 @@ File that contains some useful functions for natural language processing
 import re
 import string
 
-def clean(text, numbers = True, non_ascii = True):
+import re
+import string
+
+def clean(text, numbers = True, non_ascii = True, stop = False):
     """
     Function to clean text
 
@@ -29,13 +32,25 @@ def clean(text, numbers = True, non_ascii = True):
 
     """
     text = text.lower() # make text lower case
-    text = re.sub('[%s]' % re.escape(string.punctuation), '', text) # removes punctuation
+
+
+    # this needs fixing
+    text = re.sub(' +', ' ', text)
 
     if numbers:
         text = re.sub('\w*\d\w*', '', text) # removes numbers
 
     if non_ascii:
-        text = re.sub(r'[^\x00-\x7F]+',' ', text)
+        text = re.sub(r'[^\x00-\x7F]+',' ', text)    # remove non_ascii chars
 
-    text = re.sub('\s+',' ',text)
+    if not stop:
+
+        stop_words = set(stopwords.words('english'))
+
+        text = ' '.join([w for w in text.split() if not w in stop_words])
+
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text) # removes punctuation
+
+
+
     return text
